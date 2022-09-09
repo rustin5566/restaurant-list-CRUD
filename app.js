9e866e608e8bcbd886f73a4f25da1f2fe938c00b
rustin5566 <e0935728057@gmail.com>
2022-09-09 16:40:19 +0800

    add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
+// 載入外部套件
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 
+// mongoose 連線
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected')
+})
+
+
+// routig
 app.get('/', (req, res) => {
   res.send('hello world')
 })
---
+++ models/restaurant-list.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const restauarantListSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  nameEn: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  googole_map: {
+    type: String,
+    required: true
+  },
+  ratig: {
+    type: Number,
+    required: true
+  },
+  description: {
+    type: String,
+  }
+
+})
+
+module.exports = mongoose.model('RestaurantList', restauarantListSchema)
