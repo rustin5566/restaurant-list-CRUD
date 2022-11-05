4c8aa32c55deba8285be6c24c50ec841d64e59ab
rustin5566 <e0935728057@gmail.com>
2022-11-06 00:07:12 +0800

    add login.hbs and register.hbs and new dinerSchema

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
+const users = require('./modules/users')
 
 // home setting
 router.use('/', home)
 router.use('/restaurants', restaurants)
+router.use('/users', users)
 
 module.exports = router
