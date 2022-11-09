57facff46bf0b98a3b3185244771fc674a3a9661
rustin5566 <e0935728057@gmail.com>
2022-11-10 00:20:25 +0800

    feat: fix the bug that cant login in and add flash-connect content

--- app.js
+++ app.js
@@ -9,32 +9,21 @@ const RestaurantList = require('./models/restaurant-list')
 const methodOverride = require('method-override')
 const routes = require('./routes')
 const usePassport = require('./config/passport')
+const flash = require('connect-flash')
 
 
 require('./config/mongoose')
 
+// setting engine
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 //setting static files 能成功透過 Express 來載入 Bootstrap 與 Popper
 app.use(express.static('public'))
-
 // setting mehtodoverride
 app.use(methodOverride('_method'))
 // setting body-parser
 app.use(bodyParser.urlencoded({ extended: true }))
-
-usePassport(app)
-
-app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.isAuthenticated()
-  res.locals.user = req.user
-  next()
-})
-
-// setting routes
-app.use(routes)
-
-// setting engine
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
-app.set('view engine', 'hbs')
 
 // setting session
 app.use(session({
@@ -42,6 +31,22 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }))
+
+usePassport(app)
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
+  next()
+})
+// setting routes
+app.use(routes)
+
+
+
+
 
 
 app.listen(3000, () => {
